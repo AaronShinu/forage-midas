16bc3fe3898baf25fdd5db073852e91cfeecbcfc
Aaron-Shinu <aaronshinu23@gmail.com>
2026-07-15 17:24:24 +0100

    Completed Task 3

--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -1,14 +1,46 @@
 package com.jpmc.midascore.component;
 
+import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.entity.UserRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class TransactionListener {
+
+    private final DatabaseConduit databaseConduit;
+
+    public TransactionListener(DatabaseConduit databaseConduit) {
+        this.databaseConduit = databaseConduit;
+    }
+
     @KafkaListener(topics="${general.kafka-topic}")
     public void listen(Transaction transaction) {
-        System.out.println(transaction);
+        
+        UserRecord sender = databaseConduit.findUser(transaction.getSenderId());
+        UserRecord recipient = databaseConduit.findUser(transaction.getRecipientId());
+
+        if (sender == null || recipient == null) {
+            System.out.println("Sender or recipient not found for transaction: " + transaction);
+            return;
+        }
+
+        if (sender.getBalance() < transaction.getAmount()) {
+            System.out.println("Insufficient balance for transaction: " + transaction);
+            return;
+        }
+
+        sender.setBalance(sender.getBalance() - transaction.getAmount());
+        recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+
+        databaseConduit.save(sender);
+        databaseConduit.save(recipient);
+
+        TransactionRecord transactionRecord = new TransactionRecord(sender, recipient, transaction.getAmount());
+
+        databaseConduit.saveTransaction(transactionRecord);
+
     }
 }
--- src/test/java/com/jpmc/midascore/TaskThreeTests.java
+++ src/test/java/com/jpmc/midascore/TaskThreeTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
+import com.jpmc.midascore.repository.UserRepository;
 
 @SpringBootTest
 @DirtiesContext
@@ -23,6 +24,9 @@ public class TaskThreeTests {
     @Autowired
     private FileLoader fileLoader;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
     void task_three_verifier() throws InterruptedException {
         userPopulator.populate();
@@ -31,6 +35,9 @@ public class TaskThreeTests {
             kafkaProducer.send(transactionLine);
         }
         Thread.sleep(2000);
+        System.out.println("----------------------------------------------------------");
+        System.out.println(
+            "Waldorf's balance after all transactions are processed: " + userRepository.findById(5).getBalance());
 
 
         logger.info("----------------------------------------------------------");
